97ce7d11764acfd0cc1ed893c49439b61eac9fa2
Alejo <alejogranero@gmail.com>
2018-05-15 16:42:27 -0300

    Insert + Update listos

--- app/src/main/java/com/grafflersys/alejo/clientes/MainActivity.java
+++ app/src/main/java/com/grafflersys/alejo/clientes/MainActivity.java
@@ -1,13 +1,111 @@
 package com.grafflersys.alejo.clientes;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtNombre, edtApellido, edtDni, edtDomicilio,edtTelefono;
+    Button btAdd, btList;
+    ImageView mImageView;
+
+    final int REQUEST_CODE_GALLERY= 999;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNombre = findViewById(R.id.editTextNombre);
+        edtApellido = findViewById(R.id.editTextApellido);
+        edtDni = findViewById(R.id.DNI);
+        edtDomicilio = findViewById(R.id.DomicilioCobro);
+        edtTelefono = findViewById(R.id.telefono);
+
+        mImageView = findViewById(R.id.imageView);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_GALLERY
+                );
+            }
+        });
+
+        btAdd = findViewById(R.id.btnAdd);
+        btList = findViewById(R.id.btnList);
+
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY);
+            }
+            else {
+                Toast.makeText(this, "No hay permisos para acceder al archivo", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON) //habilita el corte de la imagen
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult resultado = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK){
+                Uri resultUri = resultado.getUri();
+                //setea la imagen seleccionada de la galería en el imageView
+                mImageView.setImageURI(resultUri);
+            }
+            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                Exception error = resultado.getError();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/grafflersys/alejo/clientes/SQLiteHelper.java
@@ -0,0 +1,65 @@
+package com.grafflersys.alejo.clientes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+public class SQLiteHelper extends SQLiteOpenHelper{
+
+
+    SQLiteHelper(Context context, String name,
+                 SQLiteDatabase.CursorFactory factory,
+                 int version){
+        super(context, name, factory, version);
+    }
+
+    public void queryData(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    public void insertData(String name, String apellido, String DNI, String domicilio, String telefono, byte[] image){
+        SQLiteDatabase database = getWritableDatabase();
+        String SQL = "INSERT INTO CLIENTES VALUES (NULL, ?, ?, ?, ?, ?,?)";
+
+        SQLiteStatement statement = database.compileStatement(SQL);
+        statement.clearBindings();
+
+        statement.bindString(1, name);
+        statement.bindString(2, apellido);
+        statement.bindString(3, DNI);
+        statement.bindString(4, domicilio);
+        statement.bindString(5, telefono);
+        statement.bindBlob(6, image);
+
+        statement.executeInsert();
+    }
+
+    public void updateData(String name, String apellido, String DNI, String domicilio, String telefono, byte[] image, int id){
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "UPDATE CLIENTES SET name =?, apellido =?, DNI=? , domicilio =?, telefono= ?, image=? WHERE id =?";
+
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.bindString(1, name);
+        statement.bindString(2, apellido);
+        statement.bindString(3, DNI);
+        statement.bindString(4, domicilio);
+        statement.bindString(5, telefono);
+        statement.bindBlob(6, image);
+        statement.bindDouble(7, (double)id);
+
+        statement.execute();
+        database.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
